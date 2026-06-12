1699575d43d7071faa4e6eaf6702f57d94e62dee
purnenduthokdar6-source <purnenduthokdar6@gmail.com>
2026-06-13 01:41:58 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,16 +25,15 @@
   /**
    * Mobile nav toggle
    */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+ document.addEventListener('click', function(e) {
+  const btn = e.target.closest('.mobile-nav-toggle');
 
-  function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
-  }
-  if (mobileNavToggleBtn) {
-    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
-  }
+  if (!btn) return;
+
+  document.body.classList.toggle('mobile-nav-active');
+  btn.classList.toggle('bi-list');
+  btn.classList.toggle('bi-x');
+});
 
   /**
    * Hide mobile nav on same-page/hash links
