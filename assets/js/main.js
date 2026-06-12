793f3b771a0912b0d7d8b6bde23bfc1292b1d5d1
purnenduthokdar6-source <purnenduthokdar6@gmail.com>
2026-06-13 01:17:09 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,13 +73,9 @@
   /**
    * Scroll top button
    */
-  let scrollTop = document.querySelector('.scroll-top');
+ let scrollTop = document.querySelector('.scroll-top');
 
-  function toggleScrollTop() {
-    if (scrollTop) {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-    }
-  }
+if (scrollTop) {
   scrollTop.addEventListener('click', (e) => {
     e.preventDefault();
     window.scrollTo({
@@ -87,6 +83,7 @@
       behavior: 'smooth'
     });
   });
+}
 
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
